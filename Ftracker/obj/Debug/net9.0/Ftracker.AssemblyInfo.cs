fd9300452f80f05ffc098a59f36cd1cda30b68ac
shota <shotalapachi13@gmail.com>
2025-08-20 22:36:04 +0400

    idk

--- Ftracker/obj/Debug/net9.0/Ftracker.AssemblyInfo.cs
+++ Ftracker/obj/Debug/net9.0/Ftracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ftracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55cf2c736b312840683d65c6419eda02894b32ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6df805b9c89d9cb28188385cb86a9d7c7ddf827e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ftracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ftracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
